2e05282163b8367be628b2e59c5d3c49c1f14cae
adamMB24-meet <139688738+adamMB24-meet@users.noreply.github.com>
2023-07-17 18:22:20 +0300

    Create pythonReview.py
    A CS lab about Python review , Y2 CS lab.

---
+++ pythonReview.py
@@ -0,0 +1,30 @@
+title = input()
+description = input()
+
+
+def create_youtube_video(title, description, likes, dislikes, comments):
+	youtube_video = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {"username": input()}} 
+def create_likes(youtube_video):
+	if (likes = 0):
+	number_likes = likes + 1
+    print(number_likes)
+    else:
+     number_likes = 0
+     return number_likes
+    return likes
+
+def create_dislikes(youtube_video):
+	if (dislikes = 0):
+		number_dislikes = dislikes + 1
+	else:
+		number_dislikes = 0
+	 return number_dislikes
+    return dislikes
+
+def create_add_comment(youtube_video):
+	username = input
+	add_comment = {"username":username , "comment_text": input()}
+
+def create_youtube_video(title, description, likes, dislikes, add_comments):
+   youtube_video = {"title": title, "description": description, "likes": likes, "dislikes": dislikes, "add_comments": add_comments }
+ 
