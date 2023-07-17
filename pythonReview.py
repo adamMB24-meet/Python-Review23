75206978d80015df37db480813c4c592be605d9f
adamMB24-meet <139688738+adamMB24-meet@users.noreply.github.com>
2023-07-17 18:40:39 +0300

    a

--- pythonReview.py
+++ pythonReview.py
@@ -2,29 +2,29 @@ title = input()
 description = input()
 
 
-def create_youtube_video(title, description, likes, dislikes, comments):
-	youtube_video = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {"username": input()}} 
-def create_likes(youtube_video):
-	if (likes = 0):
-	number_likes = likes + 1
-    print(number_likes)
-    else:
-     number_likes = 0
-     return number_likes
-    return likes
+def youtube_video(title, description, likes, dislikes, comments):
+	youtube_video = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}} 
+	return youtube_video
+def likes(youtube_video):
+	if ("likes" in youtube_video):
+		youtube_video["likes"]+= 1
+   else:
+   	"likes" = 0
+   return youtube_video
 
-def create_dislikes(youtube_video):
-	if (dislikes = 0):
-		number_dislikes = dislikes + 1
+def dislikes(youtube_video):
+	if ("dislikes" in youtube_video):
+		youtube_video['dislikes'] += 1
 	else:
-		number_dislikes = 0
-	 return number_dislikes
-    return dislikes
+		dislikes = 0
+	 	return number_dislikes
+   return youtube_video
 
-def create_add_comment(youtube_video):
-	username = input
-	add_comment = {"username":username , "comment_text": input()}
+def add_comment(youtube_video, username, comment):
+	youtube_video["comments"][username] = comment 
+	return youtube_video
+	
 
-def create_youtube_video(title, description, likes, dislikes, add_comments):
+def youtube_video(title, description, likes, dislikes, add_comments):
    youtube_video = {"title": title, "description": description, "likes": likes, "dislikes": dislikes, "add_comments": add_comments }
  
